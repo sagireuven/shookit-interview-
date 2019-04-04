acba07315585f05cc6a775d893c7d97babacbfb9
sagireuven <sagireuven007@gmail.com>
2019-04-04 22:08:58 +0300

    Add files via upload

---
+++ twitter-client.js
@@ -0,0 +1,10 @@
+const Twitter = require('twitter');
+const client = new Twitter({
+  consumer_key: 'dEvicPhAR1B7d0ILLDHVbIdOq',
+  consumer_secret: 'NESKjK2yesJkqRmBHdiVYuZthcSAHubn4cbVexKdC6YtowFRcE',
+  access_token_key: '1113476976948412419-2vZS3lTWTj8uHdeZwK3eSmIZKScQ6L',
+  access_token_secret: '7PpqQARBWq0g3RB2U5Yk8sHouyh7jrZ3r4yJsxvAQXJU3'
+});
+
+exports.client = client;
+
